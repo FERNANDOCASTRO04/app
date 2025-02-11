0b9793ec323f4c080b558a0dff044bcabdedd727
FERNANDOCASTRO04 <99416501+FERNANDOCASTRO04@users.noreply.github.com>
2025-02-11 11:38:33 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'mysql-fernandoc.alwaysdata.net'
+export const DB_HOST = process.env.DB_HOST || 'mysql-fernandocagrofer1.alwaysdata.net'
 export const DB_USER = process.env.DB_USER || 'fernandoc'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1003196927'
 export const DB_NAME = process.env.DB_NAME || 'fernandocagrofer1'
